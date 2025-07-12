3f0295def1c11acf9a1d7ffb1fc8a6c2406d7858
Tamar Sapir <Tamarsapir05@gmail.com>
2025-07-12 18:24:09 +0300

    Remove server directory from repository

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 // connect to mongo
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
@@ -17,6 +20,9 @@ mongoose.connect(process.env.MONGO_URI)
 //routes
 const authRoutes = require('./routes/auth'); 
 app.use('/', authRoutes);
+const scanRoutes = require('./routes/scan');
+app.use('/', scanRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
